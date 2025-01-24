f87852d05c6690632ab25c556c5362c577f2ab92
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-24 17:16:07 -0500

    chore: :truck: add route for updating a product by ID

--- Backend_uceShoes/productManagement/productCreation/Products/productUpdate/routes/updateProductRoute.js
+++ Backend_uceShoes/productManagement/productCreation/Products/productUpdate/routes/updateProductRoute.js
@@ -1,20 +1,11 @@
 const express = require('express');
+const { updateProduct } = require('../controllers/updateProduct');
+
 const router = express.Router();
-const productsController = require('../controllers/productsController');
 
-// Obtener todos los productos
-router.get('/', productsController.getAllProducts);
-
-// Obtener un producto por ID
-router.get('/:id', productsController.getProductById);
-
-// Crear un nuevo producto
-router.post('/', productsController.createProduct);
-
-// Actualizar un producto existente
-router.put('/:id', productsController.updateProduct);
-
-// Eliminar un producto
-router.delete('/:id', productsController.deleteProduct);
+/**
+ * PUT route for updating a product by ID.
+ */
+router.put('/api/products/:id', updateProduct);
 
 module.exports = router;
