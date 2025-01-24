a952a31d1500534656a9b84dd32bf0d471579049
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-24 12:33:30 -0500

    feat: :sparkles: Add Product model definition

--- Backend_uceShoes/productManagement/productCreation/Products/productGetById/models/Products.js
+++ Backend_uceShoes/productManagement/productCreation/Products/productGetById/models/Products.js
@@ -1,66 +1,26 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-// Define the Product model
-const Product = sequelize.define(
-  'Product',
-  {
+const Product = sequelize.define('Product', {
     productId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
     },
     productName: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.TEXT,
-    },
-    size: {
-      type: DataTypes.STRING(20),
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-    },
-    color: {
-      type: DataTypes.STRING(50),
+        type: DataTypes.DECIMAL,
+        allowNull: false,
     },
     description: {
-      type: DataTypes.TEXT,
+        type: DataTypes.TEXT,
     },
-    accessories: {
-      type: DataTypes.TEXT,
-    },
-    productCode: {
-      type: DataTypes.STRING(50),
-      unique: true,
-      allowNull: false,
-    },
-    launchDate: {
-      type: DataTypes.DATE,
-    },
-    productRegisterDate: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    brand: {
-      type: DataTypes.STRING(50),
-    },
-    productStatus: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    categoryId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  },
-  {
+}, {
     tableName: 'Products',
     timestamps: false,
-  }
-);
+});
 
 module.exports = Product;
