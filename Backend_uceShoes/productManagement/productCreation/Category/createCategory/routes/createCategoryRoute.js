26a53b393c6ad01690f1f27760ca6d378aad046f
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-26 22:48:22 -0500

    feat: :sparkles: Documentation with Swagger we change and install new things

--- Backend_uceShoes/productManagement/productCreation/Category/createCategory/routes/createCategoryRoute.js
+++ Backend_uceShoes/productManagement/productCreation/Category/createCategory/routes/createCategoryRoute.js
@@ -2,7 +2,50 @@ const express = require('express');
 const router = express.Router();
 const createCategory = require('../controllers/createCategory');
 
-// Route to create a new category
+/**
+ * @swagger
+ * /api/categories/create:
+ *   post:
+ *     summary: Create a new category
+ *     description: Creates a new category. If the name already exists, it appends a number to make it unique.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the category to create.
+ *                 example: Electronics
+ *     responses:
+ *       201:
+ *         description: Category created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 categoryId:
+ *                   type: integer
+ *                   description: The ID of the newly created category.
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   description: The unique name of the category.
+ *                   example: Electronics
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Something went wrong
+ */
 router.post('/create', createCategory);
 
 module.exports = router;
