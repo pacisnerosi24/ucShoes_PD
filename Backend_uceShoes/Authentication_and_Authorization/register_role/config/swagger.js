e37991f6a34a03a038022e9758dfdb9888b0306e
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-01-27 01:12:57 -0500

    feat: :wrench: Added the swagger configuration file

---
+++ Backend_uceShoes/Authentication_and_Authorization/register_role/config/swagger.js
@@ -0,0 +1,34 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Role Management API',
+      version: '1.0.0',
+      description: 'API for managing user roles, secured with JWT.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Documentation source
+};
+
+module.exports = swaggerJsDoc(swaggerOptions);
