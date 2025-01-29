802524fcaf7bdfc5d382d20e68f16e0ce1e59ff8
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-01-29 16:32:45 -0500

    fix: :bug: secret jwt file repair for use with environment variable

--- Backend_uceShoes/Authentication_and_Authorization/register_role/config/jwtSecret.js
+++ Backend_uceShoes/Authentication_and_Authorization/register_role/config/jwtSecret.js
@@ -1,22 +1,10 @@
-const fs = require('fs');
-const crypto = require('crypto');
-const path = require('path');
-
-const secretFilePath = path.join(__dirname, '../config/jwtSecret.json');
-
-const generateJwtSecret = () => {
-  const secret = crypto.randomBytes(64).toString('hex');
-  fs.writeFileSync(secretFilePath, JSON.stringify({ JWT_SECRET: secret }));
-  console.log('New JWT_SECRET generated and saved in config/jwtSecret.json');
-  return secret;
-};
+require('dotenv').config();
 
 const getJwtSecret = () => {
-  if (fs.existsSync(secretFilePath)) {
-    const secretData = JSON.parse(fs.readFileSync(secretFilePath, 'utf-8'));
-    return secretData.JWT_SECRET;
-  }
-  return generateJwtSecret();
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in the environment variables.");
+    }
+    return process.env.JWT_SECRET;
 };
 
 module.exports = getJwtSecret;
