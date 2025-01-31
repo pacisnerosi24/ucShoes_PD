f50aa56cbbea502ac1b1af12fd38f706673f2a3b
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-31 18:36:31 -0500

    fix: :bug: update port

--- Backend_uceShoes/productManagement/productCreation/Products/productGetAll/server.js
+++ Backend_uceShoes/productManagement/productCreation/Products/productGetAll/server.js
@@ -5,7 +5,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3004;
 
 // Middleware
 app.use(cors());
