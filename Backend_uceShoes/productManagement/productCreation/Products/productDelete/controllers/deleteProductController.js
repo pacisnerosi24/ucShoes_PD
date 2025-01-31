3bd72a98017054144970f513d1b4f110472ef27d
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-31 18:27:58 -0500

    fix: :bug: returns deleted product data and improves error messages.

--- Backend_uceShoes/productManagement/productCreation/Products/productDelete/controllers/deleteProductController.js
+++ Backend_uceShoes/productManagement/productCreation/Products/productDelete/controllers/deleteProductController.js
@@ -4,15 +4,15 @@ const deleteProduct = async (req, res) => {
   const { productId } = req.params;
 
   try {
-    const product = await Product.destroy({
-      where: { productId: productId },
-    });
+    const product = await Product.findByPk(productId);
 
-    if (product === 0) {
-      return res.status(404).json({ message: 'Product not found' });
+    if (!product) {
+      return res.status(404).json({ message: '❌ Product not found' });
     }
 
-    res.status(200).json({ message: 'Product deleted successfully' });
+    await product.destroy();
+    
+    res.status(200).json({ message: 'Product deleted successfully ' });
   } catch (error) {
     console.error('Error deleting product:', error);
     res.status(500).json({ message: 'Server error' });
