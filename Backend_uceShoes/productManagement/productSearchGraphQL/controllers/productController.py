38e21f8e78e12dd1d8eaf42edd40802895466c9a
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-31 19:46:53 -0500

    feat: :sparkles: Get products directly from MySQL

--- Backend_uceShoes/productManagement/productSearchGraphQL/controllers/productController.py
+++ Backend_uceShoes/productManagement/productSearchGraphQL/controllers/productController.py
@@ -1,58 +1,32 @@
-from graphene import ObjectType, Schema, Field, String, List
-
-
-PRODUCTS = [
-    {"id": "1", "name": "Product 1", "description": "Description 1", "price": "10.0"},
-    {"id": "2", "name": "Product 2", "description": "Description 2", "price": "20.0"},
-]
+from graphene import ObjectType, Schema, Field, String, List, ID, Float
+from sqlalchemy.orm import Session
+from config.database import SessionLocal
+from models.product import Product
 
 class ProductType(ObjectType):
-    id = String()
+    id = ID()
     name = String()
     description = String()
-    price = String()
+    price = Float()
 
 class Query(ObjectType):
-    get_product = Field(ProductType, id=String(required=True))
+    get_product = Field(ProductType, id=ID(required=True))
     get_all_products = List(ProductType)
 
+    def resolve_get_product(self, info, id):
+        session: Session = SessionLocal()
+        try:
+            product = session.query(Product).filter(Product.id == id).first()
+            return product
+        finally:
+            session.close()
 
-    def resolve_get_product(root, info, id):
-        for product in PRODUCTS:
-            if product["id"] == id:
-                return product
-        return None
+    def resolve_get_all_products(self, info):
+        session: Session = SessionLocal()
+        try:
+            return session.query(Product).all()
+        finally:
+            session.close()
 
-    def resolve_get_all_products(root, info):
-        return PRODUCTS
-
-
+# Define the schema
 schema = Schema(query=Query)
-
-def get_product_by_id(id):
-    query = f"""
-    query {{
-        getProduct(id: "{id}") {{
-            id
-            name
-            description
-            price
-        }}
-    }}
-    """
-    result = schema.execute(query)
-    return result.data.get("getProduct")
-
-def get_all_products():
-    query = """
-    query {
-        getAllProducts {
-            id
-            name
-            description
-            price
-        }
-    }
-    """
-    result = schema.execute(query)
-    return result.data.get("getAllProducts")
