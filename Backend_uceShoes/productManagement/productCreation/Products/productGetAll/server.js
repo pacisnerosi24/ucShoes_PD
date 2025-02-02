fb2e9bd3267a6d8b3a1d7fd68665cd6f4e66a8cf
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-02-02 13:07:50 -0500

    fix: :bug: import contans for conection base

--- Backend_uceShoes/productManagement/productCreation/Products/productGetAll/server.js
+++ Backend_uceShoes/productManagement/productCreation/Products/productGetAll/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const sequelize = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3004;
@@ -10,6 +11,15 @@ const PORT = process.env.PORT || 3004;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Database connected successfully');
+  } catch (error) {
+    console.error('❌ Unable to connect to the database:', error);
+  }
+};
 
 // Swagger configuration
 const swaggerOptions = {
