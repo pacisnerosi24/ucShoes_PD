eb2047d2bbc72158ee3ce77841c74fd3ff6088bf
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-02-07 16:14:58 -0500

    feat: :sparkles: Added get user roles routes file

---
+++ Backend_uceShoes/Authentication_and_Authorization/get_user_roles/routes/getUserRolesRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+const {getRoleAll,getRolById} = require('../controllers/get_user_role_controller');
+const { authenticateToken, authRole } = require('../middleware/roleMiddleware');
+const routes = express.Router();
+
+//routes.get('/Role', getRoleAll);
+
+routes.get('/role', authenticateToken, authRole('admin'),getRoleAll);
+
+routes.get('/Role:id',getRolById);
+
+module.exports = routes;
