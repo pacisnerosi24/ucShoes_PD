1aede8a788d71db0e13a9ec9c94e0a85771c5588
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-02-07 15:17:46 -0500

    test: :rocket: test deploy apigateway 07

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -48,7 +48,7 @@ setupProxy('/api/categories/delete/:id', services.deleteCategory);
 
 setupProxy('/graphql', services.graphql);
 
-// 📌 Iniciar el API Gateway  test deploy apigateway 06
+// 📌 Iniciar el API Gateway  test deploy apigateway 07
 app.listen(PORT, () => {
   console.log(`🚀 API Gateway corriendo en http://localhost:${PORT}`);
 });
