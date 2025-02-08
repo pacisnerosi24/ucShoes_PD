cb665fdcf75184e5982c09273cbfa557a13084be
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-02-07 19:40:41 -0500

    test: :bug: test 02 creation category

--- Backend_uceShoes/productManagement/productCreation/Category/createCategory/server.js
+++ Backend_uceShoes/productManagement/productCreation/Category/createCategory/server.js
@@ -8,7 +8,7 @@ const  sequelize  = require('./config/database');
 const Category = require('./models/Category');
 const PORT = process.env.PORT || 3005;
 
-// Middleware test 01 creation category 
+// Middleware test 02 creation category 
 app.use(cors());
 app.use(express.json());
 
