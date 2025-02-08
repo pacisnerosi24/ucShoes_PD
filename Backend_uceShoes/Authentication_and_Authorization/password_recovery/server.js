4d7592528858c59b6dc7ac16d37f12d8a87b3b3b
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-02-08 18:46:34 -0500

    fix: :bug: Added cors in server

--- Backend_uceShoes/Authentication_and_Authorization/password_recovery/server.js
+++ Backend_uceShoes/Authentication_and_Authorization/password_recovery/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const sequelize = require('./config/Database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
