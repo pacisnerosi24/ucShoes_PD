358b8d42c0c2dd2576639a5ca81eb33031bc23b1
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-02-07 22:56:46 -0500

    test: :test_tube: teste action get user role

--- Backend_uceShoes/Authentication_and_Authorization/get_user_roles/routes/getUserRolesRoutes.js
+++ Backend_uceShoes/Authentication_and_Authorization/get_user_roles/routes/getUserRolesRoutes.js
@@ -5,6 +5,7 @@ const { authenticateToken, authRole } = require('../middleware/roleMiddleware');
 const routes = express.Router();
 
 //routes.get('/Role', getRoleAll);
+//test
 
 routes.get('/role', authenticateToken, authRole('admin'),getRoleAll);
 
