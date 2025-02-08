10ba9bad67161f0327a82e6c742fb9da1d4c4d19
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-02-08 17:35:17 -0500

    test: :test_tube: test deploy login

--- Backend_uceShoes/Authentication_and_Authorization/login_user/server.js
+++ Backend_uceShoes/Authentication_and_Authorization/login_user/server.js
@@ -19,6 +19,8 @@ app.use(cors({
   allowedHeaders: "Content-Type,Authorization"
 }));
 
+//test
+
 app.use(bodyParser.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
