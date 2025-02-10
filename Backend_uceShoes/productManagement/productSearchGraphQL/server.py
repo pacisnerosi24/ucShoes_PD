879d74d4e9a82c589d65102716b1b84256b5178e
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-02-10 17:45:30 -0500

    test: :rocket: deploy GrahQL test 01

--- Backend_uceShoes/productManagement/productSearchGraphQL/server.py
+++ Backend_uceShoes/productManagement/productSearchGraphQL/server.py
@@ -30,4 +30,4 @@ def playground():
     return send_from_directory("static", "playground.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3010)  # 🔥 deploy GrahQL test01
+    app.run(debug=True, port=3010)  # 🔥 deploy GrahQL test 01
